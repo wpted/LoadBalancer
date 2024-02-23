837e661ebc3a8e7cfbef56aaf65c30340c42f3c4
wpted <silverhandsome@gmail.com>
2024-02-23 14:54:39 +0800

    Feat: Read from command line arguments instead of user input.

--- backend_server/src/main.rs
+++ backend_server/src/main.rs
@@ -1,12 +1,29 @@
+use std::env;
 use ::axum::{routing::get, Router};
 
 #[tokio::main]
 async fn main() {
+    let args: Vec<String> = env::args().collect();
+
+    // Default host and port values if not provided as command-line arguments
+    let mut host = "127.0.0.1";
+    let mut port = 1080;
+
+    if args.len() > 1 {
+        host = &args[1];
+    }
+
+    if args.len() > 2 {
+        port = args[2].parse().expect("Invalid port number");
+    }
+
+    let endpoint = format!("{}:{}", host, port);
+
     let app: Router = Router::new()
         .route("/", get(handler))
         .route("/health", get(ok));
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:1080").await.unwrap();
+    let listener = tokio::net::TcpListener::bind(endpoint).await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
 
