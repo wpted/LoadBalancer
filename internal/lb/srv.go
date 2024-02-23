9c46f854b4a9cdc692793496bd0f314cbdead94e
wpted <silverhandsome@gmail.com>
2024-02-23 15:44:21 +0800

    Feat: Create struct RegisterRequest. Implement method Register().

--- internal/lb/srv.go
+++ internal/lb/srv.go
@@ -1,6 +1,7 @@
 package lb
 
 import (
+    "encoding/json"
     "net/http"
     "sync"
 )
@@ -19,5 +20,28 @@ func New() *LoadBalancer {
     return &LoadBalancer{
         AliveServers: make(map[string]struct{}),
         DownServers:  make(map[string]struct{}),
+    }
+}
+
+// RegisterRequest is used for registering backend servers.
+type RegisterRequest struct {
+    Address string `json:"address"`
+}
+
+// Register is a handler that is used by endpoint '/register'.
+func (l *LoadBalancer) Register(w http.ResponseWriter, req *http.Request) {
+    var p RegisterRequest
+    decoder := json.NewDecoder(req.Body)
+    if err := decoder.Decode(&p); err != nil {
+        // Return error message.
+    }
+    // Ping the address.
+    serverAlive := l.healthCheck(p.Address)
+
+    l.RLock()
+    defer l.RUnlock()
+    // Only register server when backend server is alive.
+    if serverAlive {
+        l.AliveServers[p.Address] = struct{}{}
     }
 }
