19188ac392fbc5dc6ef6433ade88e3df6eb77b96
wpted <silverhandsome@gmail.com>
2024-02-25 19:45:17 +0800

    Feat: Implement graceful shutdown and serverScan.

--- cmd/main.go
+++ cmd/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
     "LoadBalancer/internal/lb"
+    "errors"
+    "log"
     "net/http"
+    "os"
+    "os/signal"
+    "syscall"
+    "time"
 )
 
 func main() {
@@ -10,8 +16,23 @@ func main() {
     srv.HandleFunc("/", srv.Forward)
     srv.HandleFunc("/register", srv.Register)
 
-    err := http.ListenAndServe(":80", srv)
-    if err != nil {
-        panic(err)
-    }
+    go func() {
+        if err := http.ListenAndServe(":80", srv); !errors.Is(err, http.ErrServerClosed) {
+            log.Fatalf("Load balancer server error: %v", err)
+        }
+    }()
+
+    go srv.ServerScan()
+
+    sigChan := make(chan os.Signal, 1)
+    signal.Notify(sigChan, syscall.SIGINT, syscall.SIGTERM)
+    // Wait for os signal to come in.
+    <-sigChan
+    log.Println("Shutting down load balancer.")
+
+    // Wait for 5 seconds.
+    time.Sleep(5 * time.Second)
+
+    srv.Close()
+    log.Println("Load balancer shut down succeeded.")
 }
