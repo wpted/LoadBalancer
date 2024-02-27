5be53e95f8b3eb437e18bb2b7d1909e8c58c3ec2
wpted <silverhandsome@gmail.com>
2024-02-27 14:37:23 +0800

    Test: Create tests for Sticky Round-Robin Algorithm.

---
+++ internal/lbalgo/stickyRoundRobin_test.go
@@ -0,0 +1,90 @@
+package lbalgo
+
+import (
+    "LoadBalancer/internal/lb"
+    "net/http"
+    "testing"
+)
+
+func TestSRR_ChooseServer(t *testing.T) {
+    bes := lb.BEServers{
+        "Address A": struct{}{},
+        "Address B": struct{}{},
+        "Address C": struct{}{},
+        "Address D": struct{}{},
+    }
+
+    srr := NewSRR(bes)
+
+    testCases := []struct {
+        clientReq      *http.Request
+        expectedChosen string
+    }{
+        {clientReq: &http.Request{RemoteAddr: "10.0.0.1"}},
+        {clientReq: &http.Request{RemoteAddr: "10.0.0.2"}},
+        {clientReq: &http.Request{RemoteAddr: "10.0.0.3"}},
+    }
+
+    for _, tc := range testCases {
+        chosen, err := srr.ChooseServer(tc.clientReq)
+        if err != nil {
+            t.Errorf("error choosing server: got %#v.\n", err)
+        }
+
+        // Set the first chosen server to test case.
+        tc.expectedChosen = chosen
+
+        // Choose again and compare.
+        chosen, err = srr.ChooseServer(tc.clientReq)
+        if err != nil {
+            t.Errorf("error choosing server: got %#v.\n", err)
+        }
+
+        if chosen != tc.expectedChosen {
+            t.Errorf("error choosing server: expected %s, got %s.\n", tc.expectedChosen, chosen)
+        }
+    }
+}
+
+func TestSRR_Renew(t *testing.T) {
+    bes := lb.BEServers{
+        "Address A": struct{}{},
+        "Address B": struct{}{},
+        "Address C": struct{}{},
+        "Address D": struct{}{},
+    }
+
+    srr := NewSRR(bes)
+
+    allClients := Clients{
+        "10.0.0.1": "Address A",
+        "10.0.0.2": "Address B",
+        "10.0.0.3": "Address C",
+    }
+
+    srr.AllClients = allClients
+
+    newBes := lb.BEServers{
+        "Address B": struct{}{},
+        "Address C": struct{}{}, // Delete server A, D.
+        "Address E": struct{}{}, // Add server E.
+    }
+
+    srr.Renew(newBes)
+
+    for client, originalBackendServer := range allClients {
+        if _, ok := newBes[originalBackendServer]; ok {
+            // Healthy server should remain.
+            updatedServer := srr.AllClients[client]
+            if updatedServer != originalBackendServer {
+                t.Errorf("error renewing server: expected %s, got %s.\n", originalBackendServer, updatedServer)
+            }
+        } else {
+            // Unhealthy server should be updated with the next element in queue.
+            updatedServer := srr.AllClients[client]
+            if updatedServer == originalBackendServer {
+                t.Errorf("error renewing server: should be except but %s.\n", originalBackendServer)
+            }
+        }
+    }
+}
