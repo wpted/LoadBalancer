fe043020b73c94f832c6475b5dab50d4e3bc32e7
wpted <silverhandsome@gmail.com>
2024-02-27 15:23:12 +0800

    Fix: Move misplaced code from weightedRoundRobin_test.go to weightedRoundRobin.go.

--- internal/lbalgo/weightedRoundRobin.go
+++ internal/lbalgo/weightedRoundRobin.go
@@ -1,1 +1,17 @@
 package lbalgo
+
+import (
+    "LoadBalancer/internal/lb"
+    "net/http"
+)
+
+type WRR struct {
+}
+
+func (w *WRR) ChooseServer(req *http.Request) (string, error) {
+    return "", nil
+}
+
+func (w *WRR) Renew(servers lb.BEServers) {
+
+}
--- internal/lbalgo/weightedRoundRobin_test.go
+++ internal/lbalgo/weightedRoundRobin_test.go
@@ -1,17 +1,1 @@
 package lbalgo
-
-import (
-    "LoadBalancer/internal/lb"
-    "net/http"
-)
-
-type WRR struct {
-}
-
-func (w *WRR) ChooseServer(req *http.Request) (string, error) {
-    return "", nil
-}
-
-func (w *WRR) Renew(servers lb.BEServers) {
-
-}
