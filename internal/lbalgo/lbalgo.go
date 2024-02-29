ca88f23fd2d821f970ff5165a5695e550930f323
wpted <silverhandsome@gmail.com>
2024-02-29 17:37:23 +0800

    Feat: Implement function ChooseAlgo().

--- internal/lbalgo/lbalgo.go
+++ internal/lbalgo/lbalgo.go
@@ -1,14 +1,39 @@
 package lbalgo
 
 import (
-    "LoadBalancer/internal/lb"
+    "LoadBalancer/internal/model"
     "errors"
     "net/http"
 )
 
+const (
+    LeastConnection    = "LC"
+    RoundRobin         = "RR"
+    StickyRoundRobin   = "SRR"
+    WeightedRoundRobin = "WRR"
+)
+
+var (
+    ErrNoServer    = errors.New("error no available server")
+    ErrUnknownAlgo = errors.New("error unknown algorithm")
+)
+
 type LBAlgo interface {
     ChooseServer(req *http.Request) (string, error)
-    Renew(servers lb.BEServers)
+    Renew(servers model.BEServers)
 }
 
-var ErrNoServer = errors.New("error no available server")
+func ChooseAlgo(algoBrief string) (LBAlgo, error) {
+    switch algoBrief {
+    case LeastConnection:
+        return NewLC(nil), nil
+    case RoundRobin:
+        return NewRR(nil), nil
+    case StickyRoundRobin:
+        return NewSRR(nil), nil
+    case WeightedRoundRobin:
+        return NewWRR(nil), nil
+    default:
+        return nil, ErrUnknownAlgo
+    }
+}
